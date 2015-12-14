d8120146cd605728e96febbbc0160173ad57a947
mefetran <necroktan@gmail.com>
2015-12-14 18:12:12 +0300

    Starting course

---
+++ C-sharp-course/C-sharp-course/C-sharp-course/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+class Animal // Определяем класс 'Animal'
+{
+    static void Main() // Определяем метод 'Main' - точку входа в программу
+    {
+        string myVariable = "...идёт работа консоли"; // Создание переменной типа 'string' с текстовыми данными в ней
+        System.Console.WriteLine(myVariable);
+        System.Console.WriteLine(myVariable);
+        System.Console.WriteLine(myVariable); // Вывод на консоль
+        System.Console.Read(); // Перехват информации введённой с клавиатуры
+    }
+}
