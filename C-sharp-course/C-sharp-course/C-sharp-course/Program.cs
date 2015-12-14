2732f2d69535efb152b1819f58862916dce5d6b3
mefetran <necroktan@gmail.com>
2015-12-14 18:13:45 +0300

    a little bit

--- C-sharp-course/C-sharp-course/C-sharp-course/Program.cs
+++ C-sharp-course/C-sharp-course/C-sharp-course/Program.cs
@@ -6,7 +6,7 @@ class Animal // Определяем класс 'Animal'
     {
         string myVariable = "...идёт работа консоли"; // Создание переменной типа 'string' с текстовыми данными в ней
         System.Console.WriteLine(myVariable);
-        System.Console.WriteLine(myVariable);
+        System.Console.WriteLine(myVariable); // Повторяем инструкцию несколько раз.
         System.Console.WriteLine(myVariable); // Вывод на консоль
         System.Console.Read(); // Перехват информации введённой с клавиатуры
     }
