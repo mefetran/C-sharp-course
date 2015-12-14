f4d8f009ef26393b1c149b391d9351ece4aeffec
mefetran <necroktan@gmail.com>
2015-12-14 21:19:17 +0300

    using System

--- C-sharp-course/C-sharp-course/C-sharp-course/Program.cs
+++ C-sharp-course/C-sharp-course/C-sharp-course/Program.cs
@@ -1,13 +1,13 @@
-﻿
+﻿using System; // Использую пространство имён 'System'
 
 class Animal // Определяем класс 'Animal'
 {
     static void Main() // Определяем метод 'Main' - точку входа в программу
     {
         string myVariable = "...идёт работа консоли"; // Создание переменной типа 'string' с текстовыми данными в ней
-        System.Console.WriteLine(myVariable);
-        System.Console.WriteLine(myVariable); // Повторяем инструкцию несколько раз.
-        System.Console.WriteLine(myVariable); // Вывод на консоль
-        System.Console.Read(); // Перехват информации введённой с клавиатуры
+        Console.WriteLine(myVariable);
+        Console.WriteLine(myVariable); // Повторяем инструкцию несколько раз.
+        Console.WriteLine(myVariable); // Вывод на консоль
+        Console.Read(); // Перехват информации введённой с клавиатуры
     }
 }
